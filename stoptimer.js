a9b0e41b9c0ac0c5ebb04a08e37476be9ff534fd
Victor Martinez <knoopx@gmail.com>
2021-07-14 21:08:16 +0200

    intial import

---
+++ stoptimer.js
@@ -0,0 +1,42 @@
+module.exports = (RED) => {
+  class StopTimer {
+    constructor(config) {
+      RED.nodes.createNode(this, config)
+      this.on("input", this.onInput)
+      this.startTime = null
+    }
+
+    onInput = (msg) => {
+      if (msg.stop) {
+        this.stop(msg)
+      } else if (msg.reset) {
+        this.startTime = null
+        this.status("reset")
+      } else if (!this.startTime) {
+        this.startTime = Date.now()
+        this.status("start")
+        this.reportElapsedTime()
+      }
+    }
+
+    stop = (msg) => {
+      this.startTime = null
+      this.send({ ...msg, payload: this.elapsedTime() })
+    }
+
+    reportElapsedTime = () => {
+      setTimeout(() => {
+        if (this.startTime) {
+          this.status(`${Math.round(this.elapsedTime() / 1000)}s`)
+          this.reportElapsedTime()
+        }
+      }, 1000)
+    }
+
+    elapsedTime = () => {
+      return Date.now() - this.startTime
+    }
+  }
+
+  RED.nodes.registerType("stoptimer", StopTimer)
+}
